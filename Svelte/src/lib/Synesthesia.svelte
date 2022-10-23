d1ecb361eb70462d16642e3ffca3ccfd51459ed6
Fennec2000GH <qcaijun2013@gmail.com>
2022-10-23 07:01:14 -0400

    word cloud synesthesia based on word length

--- Svelte/src/lib/Synesthesia.svelte
+++ Svelte/src/lib/Synesthesia.svelte
@@ -1,16 +1,49 @@
 <script lang="ts">
     // imported assets
     import { Button } from 'sveltestrap';
+    import WordCloud from 'wordcloud';
+    // import createGraph from 'ngraph.graph';
+    // import renderGraph from 'ngraph.pixel';
+    // import nthree from 'ngraph.three';
 
-	let colorCodes = {};
-    $: numColors = Object.keys(colorCodes).length;
+    // let graph = createGraph();
+    // let graphics = nthree(graph, {interactive: true});
+    // function createNodeUI(node) {
+    //     return {
+    //         color: 0xFF00FF,
+    //         size: 20
+    //     };
+    // };
+
     function click(){
-        alert('color coding');
+        let text: string = document.querySelector('input').value; 
+        let words: string[] = text?.split(' ');
+        let wordTuples = words .map(d => [d, d.length * 10]);
+        // alert(text);
+        // alert(words.length);
+        // alert(wordTuples[0]);
+
+        WordCloud(document.getElementById('wordcloud-canvas'), { list: wordTuples } );
+        // graph.addNode(words[0], 0);
+        // for(let i = 1; i < words.length; ++i) { 
+        //     graph.addNode(words[i], i);
+        //     graph.addLink(words[i - 1], words[i]);
+        // }
+        
+        // var renderer = renderGraph(graph, {
+        //     node: createNodeUI
+        // });
+
+        // renderer.on('nodehover', function(node) {
+        //     console.log(JSON.stringify(node));
+        // });
+        // graphics.run();
     };
 </script>
 
 <div>
     <h2>Synesthesia</h2>
 	<input type='text' id='synesthesia-input' placeholder='Enter word here...'/>
-    <Button name='submit' on:click/>
+    <Button class='primary' type='submit' name='submit' size='lg' on:click={click}>Submit</Button>
+    <canvas id='wordcloud-canvas'></canvas>
 </div>
